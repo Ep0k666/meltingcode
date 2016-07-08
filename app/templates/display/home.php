8f835e9318c0b79726d70d025efd932fb120db21
julie-mathieu <julie-creation@outlook.com>
2016-07-08 14:41:24 +0200

    modifs home

---
+++ app/templates/display/home.php
@@ -0,0 +1,44 @@
+<?php $this->layout('layout', ['title' => '']) ?>
+	<!-- NAVIGATION -->
+	<nav>
+		<div class="container">
+			<h1>Lor'N SHOP</h1>
+			<ul>
+				<li><a href="#">Accueil</a></li>
+				<li><a href="#">Boutique</a></li>
+				<li><a href="#">Produits</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+			<!-- Bouton de recherche -->
+			<!-- FR/EN -->
+			<div class="clearfix"></div>
+		</div>
+	</nav>
+
+	<!-- HEADER CAROUSEL -->
+	<header>
+		<div class="container">
+			<!-- <h2></h2> -->
+			<ul>
+				<li class="active"><a href="#"></a></li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+			</ul>
+		</div>
+	</header>
+
+	<!-- SECTION BOUTIQUE -->
+	<section>
+		<section id="boutiques">
+			<div class="container">
+				<h3>Dernières boutiques</h3>
+				<div></div>
+			</div>
+	</section>
+
+	<!-- SECTION TEAM -->
+	<section>
+		
+	</section>
