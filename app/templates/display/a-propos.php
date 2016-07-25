d30497547a690aca9685768bcbd166dd3ef8cc69
julie-mathieu <julie-creation@outlook.com>
2016-07-25 14:40:23 +0200

    modifs du lundi matin

--- app/templates/display/a-propos.php
+++ app/templates/display/a-propos.php
@@ -8,14 +8,33 @@
 		<h2>Une aventure Lorraine qui en vaut le détour</h2>
 		<h3>A propos du Projet: </h3>
 
-		<p>
-			Tout a commencé par une idée… L’idée du “mieux”, du “meilleur”, l’envie de qualité.
-			L'envie de découvrir les commerçants de notre région ainsi que leurs produits. La lorraine a de merveilleuses ressources. Pourquoi ne pas aider à développer le commerce de proximité dans notre région?
-		</p>
-		<p>
-			Chaque personne dans notre team à des attentes bien particulière en ce qui concerne sa consommation, qu'elle soit alimentaire, vestimentaire ou autre. Cependant, nous nous rejoignons tous sur le même point en ce qui concerne la possibilité de consommer "régional". </p>
-		</p>
+	    <!-- <article id="arrow">
+	    	
+		     	<p id="arrow_content1">
+					Tout à commencé par une idée... L'idée du "mieux", du "meilleur", l'envie de qualité...</br>
 
+					En ce jour où notre monde est régi par les industriels, où la malbouffe est reine et que tout est délocalisé afin de faire toujours plus de bénéfices pour un coût moindre au détriment de la qualité, les petits commerçants se voient lésés par ces hyper-puissances commerciales. </br></br>
+					Pourtant, lorsque l'on y regarde bien, les tarifs de nos commerçants locaux ne sont pas forcément beaucoup plus élevés et la qualité est là. Cependant, l'idée de consommer local n'est encore pas assez présente dans les moeurs. Il y a aussi le manque de supports de communication de ces petites entreprises qui ne peuvent pas se permettre de faire de la publicité à grande échelle. 
+				</p>
+				
+								
+		</article>
+
+		
+
+		<article id="arrow_content2">
+
+		<div id="arrow_image1">
+		<!-- <img src="/img/apropos3.png" alt=""> -->
+		<!-- </div>
+			<p>
+					Le point commun à notre team cette envie de consommer régional. Consommer moins mais consommer mieux! Faire vivre l'économie régionnale! Nous avons souhaité regrouper à un endroit et dans diverses catégories, toutes les entreprises de la région lorraine. Ce site internet permet donc aux commerçants de créer gratuitement une boutique qui présente leur entreprise et les produits qu'ils proposent, ainsi que les valeurs qu'ils prônent.
+				</p>
+				<p>
+					Pour résumer, le service que nous proposons à travers ce site permet de mettre en relation le professionnel en accord avec les attentes du client.
+				</p>
+		</article>
+		<div class="clearfix"></div> --> 
 	</div>
 </section>
 
